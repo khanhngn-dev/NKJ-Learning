091c33c58c3728b330d1cd08a607fbfb73ed603e
KhanhGN <kgnguyen@myseneca.ca>
2021-11-30 02:01:26 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/js/app.js

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+const passwords = document.querySelectorAll('.password');
+const eye = document.querySelector('i');
+
+function showPassword(eye, password) {
+  if (password.type == 'password') {
+    eye.className = 'fas fa-eye-slash';
+    password.type = 'text';
+  } else {
+    eye.className = 'fas fa-eye';
+    password.type = 'password';
+  }
+}
+
+function showPasswords() {
+  eye.addEventListener('click', function() {
+    passwords.forEach(password => showPassword(eye, password));
+  });
+}
+
+function start() {
+  showPasswords()
+}
+
+window.onload = start()
