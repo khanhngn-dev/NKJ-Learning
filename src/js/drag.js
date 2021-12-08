16a326d9c7748c93b4f24b7f04eb7faf0c3fe3e9
Trong Nhan <nhantrongho2002@gmail.com>
2021-12-08 18:36:16 +0700

    Add create.html for create flashcards

---
+++ src/js/drag.js
@@ -0,0 +1,67 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+var stop = true;
+    $(".draggable").on("drag", function (e) {
+
+        stop = true;
+        console.log(e.originalEvent.clientY)
+
+        if (e.originalEvent.clientY < 100) {
+            stop = false;
+            scroll(-100)
+        }
+
+        if (e.originalEvent.clientY > ($(window).height() - 100)) {
+            stop = false;
+            scroll(100)
+        }
+
+    });
+
+    $(".draggable").on("dragend", function (e) {
+         stop = true;
+    });
+
+    var scroll = function (step) {
+        var scrollY = $(window).scrollTop();
+        console.log(scrollY)
+        $(window).scrollTop(scrollY + step);
+    }
