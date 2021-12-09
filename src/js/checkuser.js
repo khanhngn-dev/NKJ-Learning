3894561d5db11366c734d8bba47c70461df116b0
KhanhGN <kgnguyen@myseneca.ca>
2021-12-09 16:55:22 +0700

    Added support for user profile picture

--- src/js/checkuser.js
+++ src/js/checkuser.js
@@ -9,6 +9,8 @@ const loginButton = document.querySelector('.login-button');
 const logoutButton = document.querySelector('.logout-button');
 const user = document.querySelector('.user');
 const user_name = document.querySelector('.user-name');
+const user_pfp = document.querySelector('.user-pfp');
+const default_icon = document.querySelector('.fa-user-circle');
 
 function checkLogin() {
 	onAuthStateChanged(auth, (cred) => {
@@ -16,7 +18,14 @@ function checkLogin() {
 			loginButton.style.display = 'none';
 			logoutButton.style.display = 'block';
 			user.style.display = 'flex';
-			user_name.appendChild(document.createTextNode(cred.displayName));
+			user_name.appendChild(document.createTextNode(cred.displayName || cred.email));
+			console.log(cred);
+			if (cred.photoURL) {
+				user_pfp.setAttribute('src', cred.photoURL);
+				default_icon.style.display = 'none';
+			} else {
+				user_pfp.style.display = 'none';
+			}
 		} else {
 			loginButton.style.display = 'block';
 			logoutButton.style.display = 'none';
