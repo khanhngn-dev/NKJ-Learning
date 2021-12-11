88bba4f98539265c27a12b22e37d66aaddc8fb8e
KhanhGN <kgnguyen@myseneca.ca>
2021-12-11 17:57:47 +0700

    Added click support for changing profile picture

--- src/js/profile.js
+++ src/js/profile.js
@@ -3,14 +3,25 @@ import {
 	onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
 
+import {
+	getStorage,
+	ref,
+	uploadBytes,
+} from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-storage.js';
+
 const auth = getAuth();
+const storage = getStorage();
+const pfpContainer = document.querySelector('.pfp-container');
 const mainImg = document.querySelector('.main-pfp');
 const displayName = document.querySelector('.display-name');
 const mainDiv = document.querySelector('.main');
+const infoForm = document.querySelector('#info-form');
+var info;
 
 function getUser() {
 	onAuthStateChanged(auth, (cred) => {
 		if (cred) {
+			info = cred;
 			mainImg.src = cred.photoURL;
 			displayName.innerHTML = cred.displayName;
 		} else {
@@ -20,4 +31,64 @@ function getUser() {
 	});
 }
 
-window.onload = getUser();
+function updatePreview(form, input, preview) {
+	var imgFile = input.files[0];
+	if (imgFile.type.startsWith('image/')) {
+		unlockSubmit(infoForm.querySelector('.submit'));
+		preview.innerHTML = '';
+		// Create Img element
+		var img = document.createElement('img');
+		img.file = imgFile;
+
+		// Add or replace if necessary
+		if (form.querySelector('img')) {
+			preview.replaceChild(img, form.querySelector('img'));
+		} else preview.appendChild(img);
+
+		// Read the file
+		const reader = new FileReader();
+		reader.onload = (function (aImg) {
+			return function (e) {
+				aImg.src = e.target.result;
+			};
+		})(img);
+		reader.readAsDataURL(img.file);
+		return img.file;
+	} else {
+		displayInfo('<span class="error">Please upload an image type</span>', form, preview);
+		lockSubmit(infoForm.querySelector('.submit'));
+	}
+}
+
+function updateProfileImg() {
+	const fileInput = infoForm.querySelector('#profile-picture');
+	const imgPreview = infoForm.querySelector('.preview');
+
+	var imgFile;
+
+	lockSubmit(infoForm.querySelector('.submit'));
+
+	fileInput.addEventListener('change', () => {
+		imgFile = updatePreview(infoForm, fileInput, imgPreview);
+	});
+
+	infoForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		// Update profile picture
+		uploadBytes(ref(storage, `pfp-user/${info.uid}`), imgFile).then((snapshot) => {
+			console.log(snapshot);
+			window.location.reload();
+		})
+	});
+}
+
+function profile() {
+	clickDropDown();
+	clickOverlay();
+	getUser();
+	clickDropDown(pfpContainer, infoForm);
+	updateProfileImg();
+}
+
+window.onload = profile();
