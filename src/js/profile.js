cd5545cd71cda827e20769fbb65d32a2119c7b46
KhanhGN <kgnguyen@myseneca.ca>
2021-12-13 14:40:14 +0700

    Added password reset; Only load when logged in

--- src/js/profile.js
+++ src/js/profile.js
@@ -2,6 +2,9 @@ import {
 	getAuth,
 	onAuthStateChanged,
 	updateProfile,
+	reauthenticateWithCredential,
+	EmailAuthProvider,
+	updatePassword,
 } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
 
 import {
@@ -13,23 +16,24 @@ import {
 import {
 	getFirestore,
 	doc,
-	getDocs, 
+	getDocs,
 	getDoc,
-	setDoc,
-	query,
 	collection,
-	deleteDoc
+	deleteDoc,
 } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js';
 
 const auth = getAuth();
 const storage = getStorage();
+const db = getFirestore();
+const uid = localStorage.getItem('loggedIn') || sessionStorage.getItem('loggedIn');
 const pfpContainer = document.querySelector('.pfp-container');
-const user_progress = document.querySelector(".user-progress");
+const user_progress = document.querySelector('.user-progress');
 const mainImg = document.querySelector('.main-pfp');
 const displayName = document.querySelector('.display-name');
 const mainDiv = document.querySelector('.main');
 const infoForm = document.querySelector('#info-form');
-var delete_buttons = document.querySelectorAll('.delete');
+const resetForm = document.querySelector('#reset-form');
+const resetBtn = document.querySelector('#update-password');
 var search_bar = document.querySelector('.search-bar');
 var info;
 
@@ -70,15 +74,102 @@ function checkDisplayName() {
 	saveBtn.addEventListener('click', updateDisplayName);
 }
 
-function updateProgress() {
-	const sets = document.querySelectorAll('.set');
-	sets.forEach((set) => {
-		const progress_bar = set.querySelector('.progress-bar');
-		const current_count = set.querySelector('.current-count');
-		const count = localStorage.getItem('index');
-		current_count.innerHTML = parseInt(count) + 1;
-		progress_bar.value = parseInt(count) + 1;
+async function updateProgress() {
+	var nameArray = [];
+	var dataArray = [];
+	const userRef = doc(db, 'users', uid);
+	const learningSet = collection(userRef, 'learning');
+	const querySnapshot = await getDocs(learningSet);
+	querySnapshot.forEach((doc) => {
+		doc.id, ' => ', nameArray.push(doc.id);
+		doc.id, ' => ', dataArray.push(doc.data());
 	});
+	if (nameArray.length == 0) {
+		var point_icon = document.createElement('i');
+		point_icon.className = 'fas fa-hand-point-right';
+
+		var span_text = document.createElement('span');
+		span_text.innerHTML = 'Click to create your first learning set';
+		var empty_div_text = document.createElement('a');
+		var empty_div = document.createElement('div');
+		empty_div.className = 'empty-div';
+		empty_div_text.className = 'empty-div-text';
+		empty_div_text.href = 'create.html';
+		var cat_img = document.createElement('img');
+		cat_img.className = 'cat-img';
+		cat_img.src = 'img/cat.gif';
+
+		empty_div_text.appendChild(point_icon);
+		empty_div_text.appendChild(span_text);
+		empty_div.appendChild(empty_div_text);
+		empty_div.appendChild(cat_img);
+		user_progress.appendChild(empty_div);
+		search_bar.style.display = 'none';
+	} else {
+		for (let i = 0; i < nameArray.length; i++) {
+			var current_learning_ref = doc(userRef, 'learning', nameArray[i]);
+			var learning_set_snap = await getDoc(current_learning_ref);
+			//progress bar
+			var progress_bar = document.createElement('progress');
+			progress_bar.className = 'progress-bar';
+			progress_bar.max = `${learning_set_snap.data().term.length}`;
+			progress_bar.value = `${learning_set_snap.data()['current display'] || 1}`;
+
+			//Progress count
+			var current_count = document.createElement('span');
+			current_count.innerHTML = `${learning_set_snap.data()['current display'] || 1}`;
+
+			var divider = document.createElement('span');
+			divider.innerHTML = '/';
+			var total_count = document.createElement('span');
+			total_count.innerHTML = `${learning_set_snap.data().term.length}`;
+
+			//Progress count container
+			var progress_count = document.createElement('div');
+			progress_count.className = 'progress-count';
+			progress_count.appendChild(current_count);
+			progress_count.appendChild(divider);
+			progress_count.appendChild(total_count);
+
+			//Title
+			var title = document.createElement('h1');
+			title.className = 'title';
+			title.innerHTML = nameArray[i];
+
+			//Delete button in the head for card container
+			var bin_icon = document.createElement('i');
+			bin_icon.className = 'fas fa-trash-alt';
+
+			var delete_button = document.createElement('button');
+			delete_button.className = 'delete';
+
+			delete_button.appendChild(bin_icon);
+			//Top-set container
+			var top_set_container = document.createElement('div');
+			top_set_container.className = 'top-set';
+			top_set_container.appendChild(title);
+			top_set_container.appendChild(progress_count);
+			top_set_container.appendChild(delete_button);
+
+			//Container
+			var set_container = document.createElement('div');
+			set_container.className = 'set';
+
+			set_container.appendChild(top_set_container);
+			set_container.appendChild(progress_bar);
+			user_progress.appendChild(set_container);
+
+			title.addEventListener('click', function () {
+				localStorage.setItem('learningSet', nameArray[i]);
+				window.location.assign('learning.html');
+			});
+
+			delete_button.addEventListener('click', function () {
+				user_progress.removeChild(this.parentNode.parentNode);
+				deleteDoc(doc(userRef, 'learning', `${nameArray[i]}`));
+			});
+		}
+	}
 }
 
 function updatePreview(form, input, preview) {
@@ -132,114 +223,83 @@ function updateProfileImg() {
 	});
 }
 
+function comparePassword(password, confirm, preview) {
+	if (password !== confirm) {
+		lockSubmit(resetForm.querySelector('.submit'));
+		preview.innerHTML = 'Passwords do not match';
+		preview.style.display = 'block';
+	} else {
+		unlockSubmit(resetForm.querySelector('.submit'));
+		preview.style.display = 'none';
+	}
+}
+
+function updateResetForm() {
+	resetForm.querySelector('.title').innerHTML = 'Please enter your new password';
+	const div = document.createElement('div');
+	div.className = 'form-confirm';
+	div.innerHTML = `
+		<input type="password" class="input password" id="confirm-password" minlength="6" required placeholder=" " autocomplete="new-password" />
+		<label for="confirm-password" class="label">Confirm Password</label>`;
+	resetForm.insertBefore(div, resetForm.querySelector('.submit'));
+}
+
+function resetPassword() {
+	var reAuth = false;
+	resetForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		var password = resetForm.querySelector('#password').value;
+		const preview = resetForm.querySelector('.preview');
+		const cred = EmailAuthProvider.credential(info.email, password);
+
+		if (!reAuth) {
+			reauthenticateWithCredential(info, cred)
+				.then(() => {
+					alert('You have been re-authenticated');
+					resetForm.reset();
+					updateResetForm();
+
+					const confirm = resetForm.querySelector('#confirm-password');
+					const password = resetForm.querySelector('#password');
+
+					password.addEventListener('keyup', () =>
+						comparePassword(password.value, confirm.value, preview)
+					);
+					confirm.addEventListener('keyup', () =>
+						comparePassword(password.value, confirm.value, preview)
+					);
+					reAuth = true;
+				})
+				.catch((err) => {
+					preview.innerHTML = 'Incorrect Password';
+					preview.style.display = 'block';
+				});
+			preview.style.display = 'none';
+		} else {
+			updatePassword(info, password)
+				.then(() => {
+					alert('Your password has been updated');
+					window.location.reload();
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	});
+}
+
 function profile() {
 	clickDropDown();
 	clickOverlay();
 	getUser();
-	clickDropDown(pfpContainer, infoForm);
-	updateProfileImg();	
-	checkDisplayName();
-}
-
-const db = getFirestore();
-const uid = localStorage.getItem('loggedIn');
-
-var userSnap;
-var nameArray = [];
-var dataArray = [];
-const userRef = doc(db, 'users', uid);
-const learningSet = collection(userRef, 'learning');
-const querySnapshot = await getDocs(learningSet);
-querySnapshot.forEach((doc) => {
-	doc.id, ' => ', nameArray.push(doc.id);
-	doc.id, ' => ', dataArray.push(doc.data());
-});
-if (nameArray.length == 0) {
-	var point_icon = document.createElement('i');
-	point_icon.className = 'fas fa-hand-point-right';
-
-	var span_text = document.createElement('span');
-	span_text.innerHTML = "Click to create your first learning set";
-	var empty_div_text = document.createElement('a');
-	var empty_div = document.createElement('div');
-	empty_div.className = "empty-div"
-	empty_div_text.className = 'empty-div-text'
-	empty_div_text.href = 'create.html';
-	var cat_img = document.createElement('img');
-	cat_img.className = 'cat-img'
-	cat_img.src = 'img/cat.gif'
-
-	empty_div_text.appendChild(point_icon)
-	empty_div_text.appendChild(span_text)
-	empty_div.appendChild(empty_div_text);
-	empty_div.appendChild(cat_img);
-	user_progress.appendChild(empty_div)
-	search_bar.style.display = 'none';
-}
-else {
-	for (let i=0; i<nameArray.length; i++) {
-		var current_learning_ref = doc(userRef, 'learning', nameArray[i]);
-		var learning_set_snap = await getDoc(current_learning_ref);
-		//progress bar
-		var progress_bar = document.createElement('progress')
-		progress_bar.className = "progress-bar"
-		progress_bar.max = `${learning_set_snap.data().term.length}`;
-		progress_bar.value = `${learning_set_snap.data()['current display'] || 1}`;
-	
-		//Progress count
-		var current_count = document.createElement('span')
-		current_count.innerHTML = `${learning_set_snap.data()['current display'] || 1}`;
-	
-		var divider = document.createElement('span')
-		divider.innerHTML = "/";
-		var total_count = document.createElement('span')
-		total_count.innerHTML = `${learning_set_snap.data().term.length}`;
-	
-		//Progress count container
-		var progress_count = document.createElement('div')
-		progress_count.className = "progress-count"
-		progress_count.appendChild(current_count)
-		progress_count.appendChild(divider)
-		progress_count.appendChild(total_count)
-		
-		//Title
-		var title = document.createElement('h1');
-		title.className = "title";
-		title.innerHTML = nameArray[i];
-	
-		//Delete button in the head for card container
-		var bin_icon = document.createElement('i');
-		bin_icon.className = 'fas fa-trash-alt';
-	
-		var delete_button = document.createElement('button');
-		delete_button.className = 'delete';
-	
-		delete_button.appendChild(bin_icon);
-		//Top-set container
-		var top_set_container = document.createElement('div');
-		top_set_container.className = "top-set"
-		top_set_container.appendChild(title)
-		top_set_container.appendChild(progress_count)
-		top_set_container.appendChild(delete_button)
-	
-		//Container
-		var set_container = document.createElement('div');
-		set_container.className="set";
-	
-		set_container.appendChild(top_set_container)
-		set_container.appendChild(progress_bar)
-		user_progress.appendChild(set_container)
-
-		title.addEventListener('click', function() {
-			localStorage.setItem('learningSet', nameArray[i])
-			window.location.assign('learning.html');
-		})
-
-		delete_button.addEventListener('click', function() {
-			user_progress.removeChild(this.parentNode.parentNode);
-			deleteDoc(doc(userRef, 'learning', `${nameArray[i]}`))
-		});
+	if (uid) {
+		clickDropDown(pfpContainer, infoForm);
+		clickDropDown(resetBtn, resetForm);
+		updateProfileImg();
+		checkDisplayName();
+		updateProgress();
+		resetPassword();
 	}
 }
-window.onload = profile();
 
+window.onload = profile();
