cb44dad9e3090ac1adef772c4e41ccf31017675f
KhanhGN <kgnguyen@myseneca.ca>
2021-12-15 11:11:33 +0700

    Added displayInfo()

--- src/js/main_helper.js
+++ src/js/main_helper.js
@@ -36,3 +36,14 @@ function lockSubmit(submit) {
 function unlockSubmit(submit) {
 	submit.removeAttribute('disabled');
 }
+
+function displayInfo(info, form, location) {
+	var infoMessage = info.message
+		? info.message
+				.substring(info.message.search(/\/[\w+-.]+/) + 1, info.message.search(/\)/))
+				.replace(/-/g, ' ')
+		: info;
+	location.innerHTML = infoMessage.charAt(0).toUpperCase() + infoMessage.slice(1);
+	location.style.display = 'block';
+	form.reset();
+}
